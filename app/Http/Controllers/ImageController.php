ee874dc18d267e67664f60a087b34f2c11b895b8
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-12-03 14:05:47 +0800

    updates

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class ImageController extends Controller
 {
-    //
+    /**
+     * Serve an image file or a collection of image files.
+     *
+     * @return \Symfony\Component\HttpFoundation\StreamedResponse|\Illuminate\Http\Response
+     */
+
+    public function carousel()
+    {
+        //
+    }
 }
--- routes/assets.php
+++ routes/assets.php
@@ -3,7 +3,7 @@
 /**
  * Asset Management Routes
  *
- * This file defines routes related to serving and managing assets such as images.
+ * This file defines routes related to serving and managing assets such as images and pdfs.
  * Commonly used for dynamic or static asset retrieval.
  *
  * Example routes that might be included:
@@ -14,6 +14,10 @@
  * Directory: routes/assets.php
  */
 
-// use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ImageController;
+use Illuminate\Support\Facades\Route;
 
-// Route::prefix('/image')->name('image.')->controller()
+Route::prefix('/image')->name('image.')->controller(ImageController::class)->group(function () {
+    // Returns an array of images
+    Route::get('/carousel', 'carousel')->name('carousel');
+});
