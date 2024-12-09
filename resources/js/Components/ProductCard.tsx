8dbb5dac1257a56fbaf64e61dd2f3227f4a393c9
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-12-10 01:32:34 +0800

    updates

--- resources/js/Components/ProductCard.tsx
+++ resources/js/Components/ProductCard.tsx
@@ -65,13 +65,13 @@ export default function ProductCard({ product, href }: ProductCardProps) {
     return (
         <Link
             href={href}
-            className="group flex h-[22rem] w-52 flex-col rounded-sm border-[1px] border-slate-300 p-2 shadow-md"
+            className="group flex h-[23rem] w-52 flex-col rounded-sm border-[1px] border-slate-300 p-2 shadow-md"
         >
             <div className="mb-2 flex-1">
                 <img
                     src={product.imageSrc}
                     alt={product.name}
-                    className="mb-3 aspect-[12/10] w-full object-cover"
+                    className="mb-3 aspect-square w-full object-cover"
                 />
                 <h3 className="mb-2 line-clamp-2 text-lg font-bold leading-5">
                     {product.name}
