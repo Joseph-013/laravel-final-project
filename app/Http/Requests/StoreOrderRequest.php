b5a6b627adb33f5b90c3de7478fe92aa90ed27e2
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-12-10 20:21:24 +0800

    rename

--- app/Http/Requests/StoreOrderRequest.php
+++ app/Http/Requests/StoreOrderRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use Carbon\Carbon;
 use Illuminate\Foundation\Http\FormRequest;
 
-class OrderRequest extends FormRequest
+class StoreOrderRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
